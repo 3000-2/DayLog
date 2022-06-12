36ed5a83b63b3c72e632721970ff52ca63efbfd0
3000-2 <psch0493@gmail.com>
2022-06-12 16:23:31 +0900

    init

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return <NavigationContainer></NavigationContainer>;
+}
+
+export default App;
