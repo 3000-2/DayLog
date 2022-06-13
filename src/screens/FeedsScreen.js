6efc9aee8643223ef99751871b192e6248f8a654
3000-2 <psch0493@gmail.com>
2022-06-14 00:18:49 +0900

    feedList, feedListItem

--- src/screens/FeedsScreen.js
+++ src/screens/FeedsScreen.js
@@ -2,12 +2,13 @@ import React, {useContext} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
 import logContext from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 function FeedsScreen() {
   const {logs} = useContext(logContext);
-  console.log(logs);
   return (
     <View style={styles.block}>
+      <FeedList logs={logs} />
       <FloatingWriteButton />
     </View>
   );
